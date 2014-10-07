c81601ca349c0d582dfd9d20607505ab4917efb8
Kara Lusabia <kclusabia125@gmail.com>
2014-10-07 03:34:09 -0400

    added another column. created a function to load program.

--- iProject1/dist/scripts/host/memory.js
+++ iProject1/dist/scripts/host/memory.js
@@ -9,11 +9,16 @@ var TSOS;
         Memory.newTable = function () {
             var memTable = "<table>";
             _MemoryArray = new Array();
+
             for (var i = 0; i <= _MemorySize; i += 8) {
+                // Creating a row of 256 bytes.
                 memTable += "<tr>";
                 _MemoryArray[i] = "00x" + i.toString(16);
+
+                // Printing the base address
                 memTable += "<td>" + _MemoryArray[i] + "</td>";
-                var j = i + 1;
+                var j = (i + 1) + 1;
+
                 while (j != i + 7) {
                     _MemoryArray[j] = 0;
                     memTable += "<td>" + _MemoryArray[j] + "</td>";
@@ -22,6 +27,39 @@ var TSOS;
                 memTable += "</tr>";
             }
             memTable += "</table>";
+            document.getElementById("memoryTable").innerHTML = memTable;
+        };
+
+        Memory.prototype.loadProgram = function (input) {
+            input = input.toString();
+            input = input.trim();
+            var x = 0;
+            var y = 2;
+
+            for (var row = 0; row < input.length; row += 8) {
+                for (var col = row + 1; col <= row + 7; col++) {
+                    _MemoryArray[j] = input.substring(x, y);
+                    x = x + 2;
+                    y = y + 2;
+                }
+            }
+            this.storeInMemory();
+        };
+
+        Memory.prototype.storeInMemory = function () {
+            var memTable = "<table>";
+
+            for (var i = 0; i < _MemorySize; i += 8) {
+                table += "<tr>";
+                _MemoryArray[i] = "00x" + i.toString(16);
+                table += "<td>" + _MemoryArray[i] + "</td>";
+
+                for (var j = i + 1; j <= i + 7; j++) {
+                    table += "<td>" + _MemoryArray[j] + "</td>";
+                }
+                table += "</tr>";
+            }
+            table += "</table>";
             document.getElementById("memoryTable").innerHTML = memTable;
         };
         return Memory;
--- iProject1/source/scripts/host/memory.ts
+++ iProject1/source/scripts/host/memory.ts
@@ -9,11 +9,15 @@ module TSOS {
         public static newTable() {
             var memTable = "<table>";
             _MemoryArray = new Array();
+
             for (var i = 0; i <= _MemorySize; i += 8) {
+                // Creating a row of 256 bytes.
                 memTable += "<tr>";
                 _MemoryArray[i] = "00x" + i.toString(16);
+                // Printing the base address
                 memTable += "<td>" + _MemoryArray[i] + "</td>";
-                var j = i + 1;
+                var j = (i+1) + 1;
+                // Setting the memory to 0.
                 while (j != i + 7) {
                     _MemoryArray[j] = 0;
                     memTable += "<td>" + _MemoryArray[j] + "</td>";
@@ -24,5 +28,43 @@ module TSOS {
                 memTable += "</table>";
                 document.getElementById("memoryTable").innerHTML = memTable;
         }
+
+        public loadProgram(input) {
+            input=input.toString();
+            input = input.trim();
+            var x = 0;
+            var y = 2;
+
+            for(var row = 0; row < input.length; row += 8) {
+                for(var col = row + 1; col <= row + 7; col++) {
+                    _MemoryArray[j] = input.substring(x,y);
+                    x = x+2;
+                    y = y+2;
+                }
+            }
+            this.storeInMemory();
+        }
+
+        public storeInMemory(){
+            var memTable = "<table>";
+
+            for(var i=0; i<_MemorySize;i+=8) {
+
+                table += "<tr>";
+                _MemoryArray[i] = "00x" + i.toString(16);
+                table += "<td>" + _MemoryArray[i] + "</td>";
+
+                for(var j=i+1; j<=i+7;j++) {
+                    table += "<td>" + _MemoryArray[j] + "</td>";
+                }
+                table += "</tr>";
+            }
+            table +="</table>";
+            document.getElementById("memoryTable").innerHTML = memTable;
+        }
+
+
+
+
     }
 }
