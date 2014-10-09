c48b8d5722a5bc289e4e1f366a859fcd8ac3fa8c
Kara Lusabia <kclusabia125@gmail.com>
2014-10-08 20:57:29 -0400

    messing around with the memory table to make it look good

--- iProject1/source/scripts/os/processControlBlock.ts
+++ iProject1/source/scripts/os/processControlBlock.ts
@@ -8,11 +8,17 @@ module TSOS {
         constructor() {
     }
 
-        public newPCB(pid, status, initMem, finalMem) {
+        public newPCB(pid, status, pc, initMem, finalMem) {
             this.pid = pid;
             this.status = status;
+            this.pc = pc;
             this.initMem = initMem;
             this.finalMem = finalMem;
+
+            this.acc = 0;
+            this.xReg = 0;
+            this.yReg = 0;
+            this.zReg = 0;
         }
     }
 }
