0265f3de7dea3c060a93b0bd6a41fb77f0229f6a
Kara Lusabia <kclusabia125@gmail.com>
2014-10-12 00:25:04 -0400

    opcodes can now be loaded into memory!

--- iProject1/dist/scripts/host/memory.js
+++ iProject1/dist/scripts/host/memory.js
@@ -13,13 +13,12 @@ var TSOS;
             for (var i = 0; i <= _MemorySize; i += 8) {
                 // Creating a row of 256 bytes.
                 memTable += "<tr>";
-                _MemoryArray[i] = i.toString(10);
 
-                // Printing the base address
-                memTable += "<td>" + _MemoryArray[i] + "</td>";
+                //Base
+                memTable += "<td>" + "00x" + i.toString(16);
                 var j = i;
 
-                while (j != i + 8) {
+                while (j <= i + 7) {
                     _MemoryArray[j] = j;
                     memTable += "<td>" + _MemoryArray[j] + "</td>";
                     j++;
@@ -33,17 +32,22 @@ var TSOS;
         Memory.prototype.loadProgram = function (xth) {
             // input=input.toString();
             //TODO change function
-            input = xth.trim();
             var input = xth.replace(/^\s+|\s+$/g, '');
+            input = xth.trim(); //
 
-            alert(input.toString() + input.length);
             var x = 0;
             var y = x + 2;
             for (var row = 0; row < input.length / 2; row += 8) {
-                for (var col = row + 1; col <= row + 7; col++) {
+                for (var col = row; col <= row + 7; col++) {
                     _MemoryArray[col] = input.substring(x, y);
                     x = y + 1;
                     y = y + 3;
+
+                    if (_MemoryArray[col] == "") {
+                        _MemoryArray[col] = col;
+                        x;
+                        break;
+                    }
                 }
             }
             this.storeInMemory();
@@ -54,10 +58,11 @@ var TSOS;
 
             for (var i = 0; i <= _MemorySize; i += 8) {
                 memTable += "<tr>";
-                memTable += "<td>" + _MemoryArray[i] + "</td>";
-
-                for (var j = i + 1; j <= i + 9; j++) {
+                memTable += "<td>" + "00x" + i.toString(16) + "</td>";
+                var j = i;
+                while (j <= i + 7) {
                     memTable += "<td>" + _MemoryArray[j] + "</td>";
+                    j++;
                 }
                 memTable += "</tr>";
             }
