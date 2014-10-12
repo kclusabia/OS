d9bcfd7f650d57f9de94c17be3bcabd4c34297e6
Kara Lusabia <kclusabia125@gmail.com>
2014-10-12 02:05:01 -0400

    returning the content at that PC

--- iProject1/dist/scripts/os/memoryManager.js
+++ iProject1/dist/scripts/os/memoryManager.js
@@ -1,3 +1,16 @@
 /**
 * Created by kara2 on 10/8/14.
 */
+var TSOS;
+(function (TSOS) {
+    var memoryManager = (function () {
+        function memoryManager() {
+            memory = new TSOS.Memory();
+        }
+        memoryManager.prototype.readMemory = function (pc) {
+            return _MemoryArray[pc];
+        };
+        return memoryManager;
+    })();
+    TSOS.memoryManager = memoryManager;
+})(TSOS || (TSOS = {}));
--- iProject1/source/scripts/os/memoryManager.ts
+++ iProject1/source/scripts/os/memoryManager.ts
@@ -1,3 +1,16 @@
 /**
  * Created by kara2 on 10/8/14.
  */
+module TSOS {
+
+    export class memoryManager {
+
+        constructor() {
+            memory = new Memory();
+        }
+
+        public readMemory(pc) {
+            return _MemoryArray[pc];
+        }
+    }
+}
