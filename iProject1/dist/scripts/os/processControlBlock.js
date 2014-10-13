9cab6e42fe8db4f63c17417d5a19779c85dd4513
Kara Lusabia <kclusabia125@gmail.com>
2014-10-12 21:43:32 -0400

    had to add more parameters

--- iProject1/dist/scripts/os/processControlBlock.js
+++ iProject1/dist/scripts/os/processControlBlock.js
@@ -6,17 +6,39 @@ var TSOS;
 (function (TSOS) {
     var ProcessControlBlock = (function () {
         function ProcessControlBlock() {
-        }
-        ProcessControlBlock.prototype.newPCB = function (pid, base, limit) {
-            this.pid = pid;
-            this.base = base;
-            this.limit = limit;
-
+            this.base = 0;
+            this.limit = "";
             this.acc = 0;
             this.xReg = 0;
             this.yReg = 0;
             this.zReg = 0;
+        }
+        ProcessControlBlock.prototype.newPCB = function (base, limit) {
+            this.base = base;
+            this.limit = limit;
+            this.incrementPID();
         };
+
+        ProcessControlBlock.prototype.showPCB = function () {
+            document.getElementById("PID").innerHTML = this.pid;
+            document.getElementById("PC1").innerHTML = this.pc;
+            document.getElementById("Acc1").innerHTML = this.acc;
+            document.getElementById("Base").innerHTML = this.base;
+            document.getElementById("Limit").innerHTML = this.limit;
+            document.getElementById("XReg1").innerHTML = this.xReg;
+            document.getElementById("YReg1").innerHTML = this.yReg;
+            document.getElementById("ZReg1").innerHTML = this.zReg;
+        };
+
+        ProcessControlBlock.prototype.incrementPID = function () {
+            ProcessControlBlock.pid++;
+        };
+
+        ProcessControlBlock.prototype.getPID = function () {
+            return ProcessControlBlock.pid;
+        };
+        ProcessControlBlock.pid = -1;
+        ProcessControlBlock.pc = 0;
         return ProcessControlBlock;
     })();
     TSOS.ProcessControlBlock = ProcessControlBlock;
--- iProject1/source/scripts/os/processControlBlock.ts
+++ iProject1/source/scripts/os/processControlBlock.ts
@@ -5,18 +5,42 @@ various processes.
  ------------ */
 module TSOS {
     export class ProcessControlBlock {
-        constructor() {
-    }
 
-        public newPCB(pid, base, limit) {
-            this.pid = pid;
+        public static pid = -1;
+        public static pc = 0;
+        public base:number = 0;
+        public limit = "";
+        public acc = 0;
+        public xReg = 0;
+        public yReg = 0;
+        public zReg = 0;
+
+        constructor() {
+         }
+
+        public newPCB(base:number, limit:string):void {
             this.base = base;
             this.limit = limit;
+            this.incrementPID();
+        }
 
-            this.acc = 0;
-            this.xReg = 0;
-            this.yReg = 0;
-            this.zReg = 0;
+        public showPCB() {
+            document.getElementById("PID").innerHTML = this.pid;
+            document.getElementById("PC1").innerHTML = this.pc;
+            document.getElementById("Acc1").innerHTML = this.acc;
+            document.getElementById("Base").innerHTML = this.base;
+            document.getElementById("Limit").innerHTML = this.limit;
+            document.getElementById("XReg1").innerHTML = this.xReg;
+            document.getElementById("YReg1").innerHTML = this.yReg;
+            document.getElementById("ZReg1").innerHTML = this.zReg;
+        }
+
+        public incrementPID():void {
+            ProcessControlBlock.pid++;
+        }
+
+        public getPID() :number{
+            return ProcessControlBlock.pid;
         }
     }
 }
