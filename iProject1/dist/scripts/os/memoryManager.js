e31121dd17b011a914f612f8bbd856c959187f05
Kara Lusabia <kclusabia125@gmail.com>
2014-10-15 20:37:03 -0400

    comments! comments! comments!

--- iProject1/dist/scripts/os/memoryManager.js
+++ iProject1/dist/scripts/os/memoryManager.js
@@ -4,16 +4,23 @@
 var TSOS;
 (function (TSOS) {
     var MemoryManager = (function () {
+        // Creates a memory object. The memory table is also created.
         function MemoryManager() {
             memory = new TSOS.Memory();
             memory.newTable();
         }
+        // Reads the data from the specified address, denoted as the index.
         MemoryManager.prototype.readMemory = function (index) {
             return memory.readMem(index);
         };
 
+        // Stores the data into the specified address, denoted by the index.
         MemoryManager.prototype.storeData = function (index, data) {
             memory.storeData(index, data);
+        };
+
+        MemoryManager.prototype.updateMemory = function () {
+            return memory.updateMem();
         };
         return MemoryManager;
     })();
--- iProject1/source/scripts/os/memoryManager.ts
+++ iProject1/source/scripts/os/memoryManager.ts
@@ -5,17 +5,24 @@ module TSOS {
 
     export class MemoryManager {
 
+        // Creates a memory object. The memory table is also created.
         constructor() {
             memory = new Memory();
             memory.newTable();
         }
 
+        // Reads the data from the specified address, denoted as the index.
         public readMemory(index) {
             return memory.readMem(index);
         }
 
+        // Stores the data into the specified address, denoted by the index.
         public storeData(index, data) {
             memory.storeData(index, data);
+        }
+
+        public updateMemory() {
+            return memory.updateMem();
         }
     }
 }
