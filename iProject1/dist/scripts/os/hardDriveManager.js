16bd6470e96134cfed534173d52d8cef05f30490
Kara Lusabia <kclusabia125@gmail.com>
2014-12-01 12:31:24 -0500

    used local storage to create file system

--- iProject1/dist/scripts/os/hardDriveManager.js
+++ iProject1/dist/scripts/os/hardDriveManager.js
@@ -9,30 +9,50 @@ var TSOS;
             this.sectorSize = 8;
             this.blockSize = 8;
             this.mbr = 5;
-            this.bytesPerBlock = 60;
+            this.metaDataSize = 64;
             this.createFileSystem();
         }
+        HardDriveManager.prototype.printData = function () {
+            var dataFile = "";
+            for (var bytes = 0; bytes < this.metaDataSize; bytes++) {
+                dataFile += "0";
+            }
+            return dataFile;
+        };
         HardDriveManager.prototype.createFileSystem = function () {
             var fileSystemTable = "<table>";
+
             for (var t = 0; t < this.trackSize; t++) {
                 for (var s = 0; s < this.sectorSize; s++) {
                     for (var b = 0; b < this.blockSize; b++) {
-                        fileSystemTable += "<tr><td>" + t + s + b + " ";
+                        // TSB block acts as the key.
+                        var key = this.createKey(t, s, b);
 
-                        for (var m = 0; m < this.mbr; m++) {
-                            // fileSystemTable += "<td>";
-                            fileSystemTable += "<td>" + 0;
-                        }
+                        var _metaData = this.printData();
+                        localStorage.setItem(key, _metaData);
 
-                        for (var bytes = 1; bytes < this.bytesPerBlock; bytes++) {
-                            fileSystemTable += bytes;
-                        }
+                        var metaData = localStorage.getItem(key);
+                        var meta = metaData.substring(0, 4);
+                        var data = metaData.substring(4, this.metaDataSize);
+                        fileSystemTable += "<tr><td>" + t + s + b;
+
+                        //                        for(var m = 0; m < this.mbr; m++) {
+                        // fileSystemTable += "<td>";
+                        fileSystemTable += "<td>" + meta + "<td>" + data;
+
+                        //                        for(var bytes = 1; bytes < this.bytesPerBlock; bytes++) {
+                        //                            fileSystemTable += bytes;
+                        //                        }
                         fileSystemTable += "</td></tr>";
                         //
                     }
                 }
             }
             document.getElementById("fileSystemTable").innerHTML = fileSystemTable + "</table>";
+        };
+
+        HardDriveManager.prototype.createKey = function (t, s, b) {
+            return String(t) + String(s) + String(b);
         };
         return HardDriveManager;
     })();
--- iProject1/source/scripts/os/hardDriveManager.ts
+++ iProject1/source/scripts/os/hardDriveManager.ts
@@ -7,27 +7,44 @@ module TSOS {
         public sectorSize:number = 8;
         public blockSize:number = 8;
         public mbr:number = 5;
-        public bytesPerBlock:number = 60;
+        public metaDataSize:number = 64;
 
         constructor() {
             this.createFileSystem();
         }
 
+        public printData():string {
+        var dataFile = "";
+            for(var bytes = 0; bytes < this.metaDataSize; bytes++) {
+                dataFile  += "0";
+    }
+    return dataFile;
+    }
         public createFileSystem() {
             var fileSystemTable = "<table>";
+
             for (var t = 0; t < this.trackSize; t++) {
                 for(var s = 0; s < this.sectorSize; s++) {
                     for (var b = 0; b < this.blockSize; b++) {
-                        fileSystemTable += "<tr><td>" + t + s  + b + " ";
+                        // TSB block acts as the key.
+                        var key = this.createKey(t,s,b);
 
-                        for(var m = 0; m < this.mbr; m++) {
+                        var _metaData = this.printData();
+                        localStorage.setItem(key, _metaData);
+
+                        var metaData = localStorage.getItem(key);
+                        var meta = metaData.substring(0,4);
+                        var data = metaData.substring(4,this.metaDataSize);
+                        fileSystemTable += "<tr><td>" + t + s  + b;
+
+//                        for(var m = 0; m < this.mbr; m++) {
                            // fileSystemTable += "<td>";
-                            fileSystemTable += "<td>" + 0;
-                        }
+                            fileSystemTable += "<td>" + meta + "<td>" + data;
 
-                        for(var bytes = 1; bytes < this.bytesPerBlock; bytes++) {
-                            fileSystemTable += bytes;
-                        }
+
+//                        for(var bytes = 1; bytes < this.bytesPerBlock; bytes++) {
+//                            fileSystemTable += bytes;
+//                        }
                         fileSystemTable += "</td></tr>";
 //
                     }
@@ -35,6 +52,10 @@ module TSOS {
             }
             document.getElementById("fileSystemTable").innerHTML = fileSystemTable + "</table>";
           }
+
+        public createKey(t,s,b) {
+            return String(t) + String(s) + String(b);
+        }
         }
 
     }
